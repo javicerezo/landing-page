5e8b8335fc1e705888dc6f800a17b1a17050055f
Javi Cerezo <jc.webmob@gmail.com>
2023-06-15 12:58:37 +0200

    últimos retoques

--- src/js/app.js
+++ src/js/app.js
@@ -5,7 +5,10 @@ let contador = 0;
 // EVENTOS
 window.addEventListener('DOMContentLoaded', () => {
     cargarSeccion1();
-    
+
+    const ulBolitas = document.querySelector('.js-seccion1__ul');
+    cargarBolitas(ulBolitas);
+
     //slider de imagen
     const btnFlecha = document.querySelector('.js-seccion1__flechas');
     btnFlecha.addEventListener('click', e => {
@@ -17,6 +20,7 @@ window.addEventListener('DOMContentLoaded', () => {
                 limpiarHTML(nombre);
                 contador--;
                 moverImagen(contador, nombre, imagen);
+                moverBolita(ulBolitas, contador, 'izquierda');
             }
         }
         if(e.target.classList.contains('fa-arrow-right-long')) {
@@ -24,9 +28,12 @@ window.addEventListener('DOMContentLoaded', () => {
                 limpiarHTML(nombre);
                 contador++;
                 moverImagen(contador, nombre, imagen);
+                moverBolita(ulBolitas, contador, 'derecha');
             }
         }
     });
+
+    // boton formulario
     const btnEnviar = document.querySelector('.js-seccion5__form-boton');
     btnEnviar.addEventListener('click', (e) => {
         e.preventDefault();
@@ -52,27 +59,49 @@ function cargarSeccion1 () {
             <img src="build/img/nueva-coleccion/botas0.jpg" alt="imagen botas">
         </div>
         <div class="c-seccion1__listado">
-            <ul class="c-seccion1__ul">
-                <li class="c-seccion1__li"><i class="fa-solid fa-circle"></i></li>
-                <li class="c-seccion1__li"><i class="fa-solid fa-circle"></i></li>
-                <li class="c-seccion1__li"><i class="fa-solid fa-circle"></i></li>
+            <ul class="c-seccion1__ul js-seccion1__ul">
+
             </ul>
             <div class="c-seccion1__divisor"></div>
             <div class="c-seccion1__flechas js-seccion1__flechas">
-                <li class="c-seccion1__li"><i class="fa-solid fa-arrow-left-long"></i></li>
-                <li class="c-seccion1__li"><i class="fa-solid fa-arrow-right-long"></i></li>
+                <li class="c-seccion1__flecha"><i class="fa-solid fa-arrow-left-long"></i></li>
+                <li class="c-seccion1__flecha"><i class="fa-solid fa-arrow-right-long"></i></li>
             </div>
         </div>
     `;
     contenedorProducto.appendChild(producto);
 }
 
+function cargarBolitas (contenedor) {
+    botasColeccion.forEach( bota => {
+        const li = document.createElement('li');
+        li.classList.add('c-seccion1__li');
+        li.innerHTML = `
+            <i class="fa-solid fa-circle"></i>
+        `;
+        contenedor.appendChild(li);
+    });
+    contenedor.children[0].children[0].classList.toggle('fa-solid');
+    contenedor.children[0].children[0].classList.toggle('fa-regular');
+}
+
 function moverImagen (contador, nombreBota, imagen) {
     const nuevaBota = botasColeccion.find(element => element.id === contador);
     const {nombre, src} = nuevaBota;
     nombreBota.textContent = nombre;
-    console.log(imagen)
     imagen.setAttribute('src', src);
+}
+
+function moverBolita (contenedor, id, direccion) {
+    contenedor.children[id].children[0].classList.toggle('fa-solid');
+    contenedor.children[id].children[0].classList.toggle('fa-regular');
+    if (direccion === 'izquierda'){
+        contenedor.children[id+1].children[0].classList.toggle('fa-solid');
+        contenedor.children[id+1].children[0].classList.toggle('fa-regular');
+    } else if (direccion === 'derecha') {
+        contenedor.children[id-1].children[0].classList.toggle('fa-solid');
+        contenedor.children[id-1].children[0].classList.toggle('fa-regular');
+    }
 }
 
 function limpiarHTML (contenedor) {
